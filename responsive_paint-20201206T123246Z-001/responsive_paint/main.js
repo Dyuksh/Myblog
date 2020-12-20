e89a5e2758be32047711b9eb6a5fe577f42c7a46
Dyuksh <75571785+Dyuksh@users.noreply.github.com>
2020-12-20 20:01:15 +0530

    Add files via upload

---
+++ responsive_paint-20201206T123246Z-001/responsive_paint/main.js
@@ -0,0 +1,61 @@
+var mouseEvent = "empty";
+var last_position_of_x, last_position_of_y;
+
+    canvas = document.getElementById("myCanvas");
+    ctx = canvas.getContext("2d");
+    width_of_line = 2;
+    color = "black";
+    var width = screen.width;
+    newwidth = screen.width - 70;
+    newheight = screen.height - 300;
+    if(width<992){
+        document.getElementById("myCanvas").width = newwidth;
+        document.getElementById("myCanvas").height = newheight;
+        document.body.style.overflow = "hidden";
+        
+
+    }
+    
+    
+    canvas.addEventListener("touchstart", my_touchstart);
+    
+    function my_touchstart(e)
+    {
+        //Addictonal Activity start
+        
+        
+        //Addictonal Activity ends
+        mouseEvent = "touchstart";
+        last_position_of_x = e.touches[0].clientX - canvas.offsetLeft;
+        last_position_of_y = e.touches[0].clientY - canvas.offsetTop;
+
+    
+    }
+
+    canvas.addEventListener("touchmove", my_touchmove);
+    
+    
+    function my_touchmove(e)
+    {
+
+         current_position_of_mouse_x =e.touches[0].clientX - canvas.offsetLeft;
+         current_position_of_mouse_y = e.touches[0].clientY - canvas.offsetTop;
+
+                 ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+
+        console.log("Last position of x and y coordinates = ");
+        console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
+        ctx.moveTo(last_position_of_x, last_position_of_y);
+
+        console.log("Current position of x and y coordinates = ");
+        console.log("x  = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+        ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
+        ctx.stroke();
+        
+
+        last_position_of_x = current_position_of_mouse_x; 
+        last_position_of_y = current_position_of_mouse_y;
+    }
+
